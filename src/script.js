df6d5038eb96dd653061362c749a323053fb3746
Karo Yousefi <karoyousefi.80@gmail.com>
2024-07-03 20:32:08 +0330

    Finished HTML and CSS

---
+++ src/script.js
@@ -0,0 +1,33 @@
+const cards = {
+    ceo: {
+        name: "CEO",
+        cat: "PR&Team",
+        pic: "src/ceo",
+    },
+    marketing: {
+        name: "Marketing",
+        cat: "PR&Team",
+        pic: "src/marketing",
+    },
+    itTeam:{
+        name: "IT team",
+        cat: "PR&Team",
+        pic: "src/itTeam",
+    },
+    supportTeam: {
+        name: "Support Team",
+        cat: "PR&Team",
+        pic: "src/supportTeam",
+    },
+    hamsterBook: {
+        name: "HamsterBook",
+        cat: "PR&Team",
+        pic: "src/hamsterBook",
+    },
+    hamsterTube: {
+        name: "HamsterTube",
+        cat: "PR&Team",
+        pic: "src/hamsterTube",
+    },
+
+};
